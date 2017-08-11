85f2dd9b5b87f86737c9ed97d7fbc0d301711f81
Jeff Wong <awole20@gmail.com>
2017-08-11 12:55:52 -0700

    first implementation

--- src/auto_reloaded_repl/core.clj
+++ src/auto_reloaded_repl/core.clj
@@ -1,6 +1,40 @@
-(ns auto-reloaded-repl.core)
+(ns auto-reloaded-repl.core
+  (:require [com.stuartsierra.component :as component]
+            [suspendable.core :refer [Suspendable]]
+            [hawk.core :as hawk]
+            [clojure.tools.nrepl :as repl]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord ResetComponent [paths watcher nrepl-connection]
+  component/Lifecycle
+  (start [component]
+    (let [nrepl-port (Integer/parseInt (slurp ".nrepl-port"))
+          nrepl-connection (repl/connect :port nrepl-port)
+          watch
+          (hawk/watch!
+           [{:paths paths
+             :filter hawk/file?
+             :handler (fn [ctx e]
+                        (println "reloading system...")
+                        (-> (repl/client nrepl-connection 1000)
+                            (repl/message {:op "eval" :code "(reloaded.repl/reset)"})
+                            repl/response-values)
+                        (println "reloaded!"))}])]
+      (-> (assoc component :watcher watch)
+          (assoc :nrepl-connection nrepl-connection))))
+  (stop [component]
+    (when (:watcher component)
+      (hawk/stop! (:watcher component)))
+    (when (:nrepl-connection component)
+      (.close (:nrepl-connection component)))
+    (dissoc component :watcher :nrepl-connection))
+  Suspendable
+  (suspend [component]
+    (assoc component :suspended? true))
+  (resume [component old-component]
+    (if (:suspended? old-component)
+      (-> (assoc component :watcher (:watcher old-component))
+          (assoc :nrepl-connection (:nrepl-connection old-component)))
+      (do (when old-component (component/stop old-component))
+          (component/start component)))))
+(defn auto-reset [paths]
+  (map->ResetComponent {:paths paths}))
--- test/auto_reloaded_repl/core_test.clj
+++ test/auto_reloaded_repl/core_test.clj
@@ -2,6 +2,4 @@
   (:require [clojure.test :refer :all]
             [auto-reloaded-repl.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;;TODO - how do you test hot swap code?
